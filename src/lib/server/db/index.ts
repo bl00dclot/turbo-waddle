42a369d0b265d2e0544c8c7e858421219f7fb25e
bl00dclot <discryptment@tutamail.com>
2026-03-02 01:12:40 +0400

    DB switch to Turso

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,13 @@ import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.TURSO_DATABASE_URL && !env.DATABASE_URL) {
+	throw new Error('TURSO_DATABASE_URL or DATABASE_URL must be set');
+}
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({
+	url: env.TURSO_DATABASE_URL ?? env.DATABASE_URL!,
+	authToken: env.TURSO_AUTH_TOKEN
+});
 
 export const db = drizzle(client, { schema });
